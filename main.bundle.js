631e581c8ed584fbb235d8df97a6680c9d747a61
szczepaniakmichal <mssk87@gmail.com>
2020-02-12 23:12:42 +0100

    script detect when current header is click. Then get current list and render items.

--- main.bundle.js
+++ main.bundle.js
@@ -1,1 +1,41 @@
 "use strict";
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+var allHeaders = document.querySelectorAll(".sub_header > h3"); //pobranie wszystkich naglowkow
+
+var allList = document.querySelectorAll(".sub_header > ul"); //pobranie list
+
+var allListItem = document.querySelectorAll(".sub_header > ul > li"); //pobranie wszystkich elementow li
+
+allHeaders.forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    allList.forEach(function (el) {
+      el.style.display = 'none';
+    });
+    e.target.nextElementSibling.style.display = 'block';
+    var currentList = e.target.nextElementSibling;
+
+    var currentListChild = _toConsumableArray(currentList.children);
+
+    allListItem.forEach(function (el) {
+      el.style.display = 'none';
+    });
+
+    function renderItems() {
+      currentListChild.forEach(function (el, index) {
+        setTimeout(function () {
+          el.style.display = 'block';
+        }, index * 300);
+      });
+    }
+
+    renderItems();
+  });
+});
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
+const allHeaders = document.querySelectorAll(".sub_header > h3"); //pobranie wszystkich naglowkow
+const allList = document.querySelectorAll(".sub_header > ul"); //pobranie list
+const allListItem = document.querySelectorAll(".sub_header > ul > li"); //pobranie wszystkich elementow li
 
+allHeaders.forEach((el) => {
+    el.addEventListener('click', (e) => {
+        allList.forEach((el) => {
+            el.style.display = 'none';
+        });
+
+        e.target.nextElementSibling.style.display = 'block';
+        const currentList = e.target.nextElementSibling;
+        const currentListChild = [...currentList.children];
+        allListItem.forEach(el => {
+            el.style.display = 'none';
+        });
+
+        function renderItems() {
+            currentListChild.forEach((el, index) => {
+                setTimeout(() => {
+                    el.style.display = 'block';
+                }, index * 300)
+            })
+        }
+        renderItems();
+    })
+});
