c732b7c50aeaa65241d505fbda52d8ee14f27d25
szczepaniakmichal <mssk87@gmail.com>
2020-02-13 20:43:44 +0100

    howManyMonth() I am work?

--- main.bundle.js
+++ main.bundle.js
@@ -14,6 +14,30 @@ var allList = document.querySelectorAll(".sub_header > ul"); //pobranie list
 
 var allListItem = document.querySelectorAll(".sub_header > ul > li"); //pobranie wszystkich elementow li
 
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = allListItem[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var liItem = _step.value;
+    liItem.style.display = 'none';
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+      _iterator["return"]();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
 allHeaders.forEach(function (el) {
   el.addEventListener('click', function (e) {
     allList.forEach(function (el) {
@@ -39,3 +63,14 @@ allHeaders.forEach(function (el) {
     renderItems();
   });
 });
+var startWork = new Date('2019-07-01 00:00:00').getTime();
+var elDate = document.querySelector("body > div > div.row.content > div > div:nth-child(2) > div.col-md-12.col-lg-7.sub_header > ul > li:nth-child(3) > span");
+
+function howManyMonth() {
+  var nowTime = new Date().getTime();
+  var howMuchMonth = Math.floor(nowTime / (1000 * 60 * 60 * 24 * 30) - startWork / (1000 * 60 * 60 * 24 * 30));
+  elDate.textContent = "obecnie pracuje ".concat(howMuchMonth, " miesi\u0119cy");
+  return howMuchMonth;
+}
+
+howManyMonth();
