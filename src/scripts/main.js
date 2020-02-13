0010a3b91f5026a01696130b3d8529d736164da2
szczepaniakmichal <mssk87@gmail.com>
2020-02-13 20:35:35 +0100

    howManyMonth() I am work?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,6 +26,20 @@ allHeaders.forEach((el) => {
                 }, index * 300)
             })
         }
+
         renderItems();
     })
 });
+
+const startWork = new Date('2019-07-01 00:00:00').getTime();
+
+const elDate = document.querySelector("body > div > div.row.content > div > div:nth-child(2) > div.col-md-12.col-lg-7.sub_header > ul > li:nth-child(3) > span")
+
+function howManyMonth() {
+    const nowTime = new Date().getTime();
+    const howMuchMonth = Math.floor((nowTime / (1000 * 60 * 60 * 24 * 30)) - (startWork / (1000 * 60 * 60 * 24 * 30)));
+    elDate.textContent = `obecnie pracuje ${howMuchMonth} miesięcy`;
+    return howMuchMonth;
+}
+
+howManyMonth();
