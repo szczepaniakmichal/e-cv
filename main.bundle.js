23f24194c2ec02afe958fa0d8225252587a255c5
szczepaniakmichal <mssk87@gmail.com>
2021-01-07 19:12:23 +0100

    clean and refactor code

--- main.bundle.js
+++ main.bundle.js
@@ -8,34 +8,42 @@ function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.
 
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
-var allHeaders = document.querySelectorAll(".sub_header > h3"); //pobranie wszystkich naglowkow
+var allHeaders = document.querySelectorAll(".sub_header > h3"); //get all title headers
 
-var allList = document.querySelectorAll(".sub_header > ul"); //pobranie list
+var allList = document.querySelectorAll(".sub_header > ul"); //get list
 
-var allListItem = document.querySelectorAll(".sub_header > ul > li"); //pobranie wszystkich elementow li
+var allListItem = document.querySelectorAll(".sub_header > ul > li"); //get all items of list
 
 var showingItems = false;
 var displayTime = 300;
-var _iteratorNormalCompletion = true;
-var _didIteratorError = false;
-var _iteratorError = undefined;
+var nowTime = new Date().getTime();
+var startLearn = new Date('2016-09-12 00:00:00').getTime();
+var startInspireLabs = new Date('2019-07-01 00:00:00').getTime();
+var elStartLearn = document.querySelector(".experience .start-lern .date");
+var elInspireLabs = document.querySelector(".experience .inspire .date");
 
-try {
-  for (var _iterator = allListItem[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-    var liItem = _step.value;
-    liItem.style.display = 'none';
-  }
-} catch (err) {
-  _didIteratorError = true;
-  _iteratorError = err;
-} finally {
+function hideListItems() {
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
   try {
-    if (!_iteratorNormalCompletion && _iterator["return"] != null) {
-      _iterator["return"]();
+    for (var _iterator = allListItem[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var liItem = _step.value;
+      liItem.style.display = 'none';
     }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
   } finally {
-    if (_didIteratorError) {
-      throw _iteratorError;
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
     }
   }
 }
@@ -45,10 +53,18 @@ function renderItems(currentListChild) {
     setTimeout(function () {
       el.style.display = 'block';
     }, index * displayTime);
-    setTimeout(function () {
-      showingItems = false;
-    }, currentListChild.length * displayTime);
   });
+  setTimeout(function () {
+    showingItems = false;
+  }, currentListChild.length * displayTime);
+}
+
+function experience(nowTime, currentWork, outputSelector) {
+  var experience = Math.floor(nowTime / (1000 * 60 * 60 * 24 * 30) - currentWork / (1000 * 60 * 60 * 24 * 30));
+  var year = Math.floor(experience / 12);
+  var month = experience % 12;
+  outputSelector.textContent = "doswiadczenie ".concat(year, " rok i ").concat(month, " miesi\u0105c");
+  return experience;
 }
 
 allHeaders.forEach(function (el) {
@@ -60,25 +76,9 @@ allHeaders.forEach(function (el) {
 
     var currentListChild = _toConsumableArray(currentList.children);
 
-    allListItem.forEach(function (el) {
-      el.style.display = 'none';
-    });
+    hideListItems();
     renderItems(currentListChild);
   });
 });
-var nowTime = new Date().getTime();
-var startLern = new Date('2016-09-12 00:00:00').getTime();
-var startInspireLabs = new Date('2019-07-01 00:00:00').getTime();
-var elStartLern = document.querySelector(".experience .start-lern .date");
-var elInspireLabs = document.querySelector(".experience .inspire .date");
-
-function experience(nowTime, currentWork, outputSelector) {
-  var experience = Math.floor(nowTime / (1000 * 60 * 60 * 24 * 30) - currentWork / (1000 * 60 * 60 * 24 * 30));
-  var year = Math.floor(experience / 12);
-  var month = experience % 12;
-  outputSelector.textContent = "doswiadczenie ".concat(year, " rok i ").concat(month, " miesi\u0105c");
-  return experience;
-}
-
-experience(nowTime, startLern, elStartLern);
+experience(nowTime, startLearn, elStartLearn);
 experience(nowTime, startInspireLabs, elInspireLabs);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,22 @@
-const allHeaders = document.querySelectorAll(".sub_header > h3"); //pobranie wszystkich naglowkow
-const allList = document.querySelectorAll(".sub_header > ul"); //pobranie list
-const allListItem = document.querySelectorAll(".sub_header > ul > li"); //pobranie wszystkich elementow li
+const allHeaders = document.querySelectorAll(".sub_header > h3"); //get all title headers
+const allList = document.querySelectorAll(".sub_header > ul"); //get list
+const allListItem = document.querySelectorAll(".sub_header > ul > li"); //get all items of list
 
 let showingItems = false;
 const displayTime = 300;
 
-for (const liItem of allListItem) {
-    liItem.style.display = 'none';
+const nowTime = new Date().getTime();
+
+const startLearn = new Date('2016-09-12 00:00:00').getTime();
+const startInspireLabs = new Date('2019-07-01 00:00:00').getTime();
+
+const elStartLearn = document.querySelector(".experience .start-lern .date");
+const elInspireLabs = document.querySelector(".experience .inspire .date");
+
+function hideListItems() {
+    for (const liItem of allListItem) {
+        liItem.style.display = 'none';
+    }
 }
 
 function renderItems(currentListChild) {
@@ -14,34 +24,11 @@ function renderItems(currentListChild) {
         setTimeout(() => {
             el.style.display = 'block';
         }, index * displayTime)
-
-        setTimeout(() => {
-            showingItems = false;
-        }, currentListChild.length * displayTime);
     })
+    setTimeout(() => {
+        showingItems = false;
+    }, currentListChild.length * displayTime);
 }
-
-allHeaders.forEach((el) => {
-    el.addEventListener('click', (e) => {
-        if (showingItems) return;
-        showingItems = true;
-        e.target.nextElementSibling.style.display = 'block';
-        const currentList = e.target.nextElementSibling;
-        const currentListChild = [...currentList.children];
-        allListItem.forEach(el => {
-            el.style.display = 'none';
-        });
-        renderItems(currentListChild);
-    })
-});
-
-const nowTime = new Date().getTime();
-
-const startLern = new Date('2016-09-12 00:00:00').getTime();
-const startInspireLabs = new Date('2019-07-01 00:00:00').getTime();
-
-const elStartLern = document.querySelector(".experience .start-lern .date");
-const elInspireLabs = document.querySelector(".experience .inspire .date");
 
 function experience(nowTime, currentWork, outputSelector) {
     const experience = Math.floor((nowTime / (1000 * 60 * 60 * 24 * 30)) - (currentWork / (1000 * 60 * 60 * 24 * 30)));
@@ -51,5 +38,17 @@ function experience(nowTime, currentWork, outputSelector) {
     return experience;
 }
 
-experience(nowTime, startLern, elStartLern);
+allHeaders.forEach((el) => {
+    el.addEventListener('click', (e) => {
+        if (showingItems) return;
+        showingItems = true;
+        e.target.nextElementSibling.style.display = 'block';
+        const currentList = e.target.nextElementSibling;
+        const currentListChild = [...currentList.children];
+        hideListItems();
+        renderItems(currentListChild);
+    })
+});
+
+experience(nowTime, startLearn, elStartLearn);
 experience(nowTime, startInspireLabs, elInspireLabs);
